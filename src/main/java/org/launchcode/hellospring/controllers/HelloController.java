3183d1d3d4255200d751f102942938a8ef22e04f
ramadevimaram <ramamaram.java@gmail.com>
2020-07-23 06:21:28 -0400

    Initial commit

---
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -0,0 +1,22 @@
+package org.launchcode.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    //Handles requst at path /hello
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "Hello,Spring";
+    }
+    @PostMapping("goodbye")
+    @ResponseBody
+    public String goodbye(){
+        return "Goodbye,Spring";
+    }
+
+}
